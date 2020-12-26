880cad6e173c513b37a2d59871d4f19b80da7968
Rone Silveira <ronebs10@hotmail.com>
2020-12-26 15:04:13 -0300

    creating  global styled components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,12 @@ import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact comnponent={Dashboard} />
-    <Route path="/repository" comnponent={Repository} />
-  </Switch>
+  <>
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/repository" component={Repository} />
+    </Switch>
+  </>
 );
 
 export default Routes;
